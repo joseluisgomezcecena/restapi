04e60fc544ecc0a577b570619af3ddd14f113391
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2021-03-17 17:47:18 -0700

    added sessions file

---
+++ v1/controller/sessions.php
@@ -0,0 +1,76 @@
+<?php
+
+require_once 'db.php';
+require_once '../model/response.php';
+
+try{
+    $writeDB = DB::connectWriteDB();
+}
+catch(PDOException $ex){
+    error_log('Connection error' . $ex, 0);
+    $response = new Response();
+    $response->setHttpStatusCode(500);
+    $response->setSuccess(false);
+    $response->addMessage('Database connection error.');
+    $response->send();
+    exit;    
+}
+
+if(array_key_exists('sessionid', $_GET)){
+
+}
+elseif(empty($_GET)){
+
+
+    if($_SERVER['REQUEST_METHOD'] !== 'POST'){
+        $response = new Response();
+        $response->setHttpStatusCode(400);
+        $response->setSuccess(false);
+        $response->addMessage('Request Method not Allowed.');
+        $response->send();
+        exit;
+    }
+
+
+    //against bruteforcer
+    sleep(1);
+
+    if($_SERVER['CONTENT_TYPE'] !== 'application/json'){
+        $response = new Response();
+        $response->setHttpStatusCode(400);
+        $response->setSuccess(false);
+        $response->addMessage('Content type header not Allowed.');
+        $response->send();
+        exit;
+    }
+
+    $raw_post_data = file_get_contents('php://input');
+
+    if(!$json_data = json_decode($raw_post_data)){
+        $response = new Response();
+        $response->setHttpStatusCode(400);
+        $response->setSuccess(false);
+        $response->addMessage('Content type not Allowed.');
+        $response->send();
+        exit;
+    }
+
+    if(isset($json_data->username) || isset($json_data->password)){
+        $response = new Response();
+        $response->setHttpStatusCode(400);
+        $response->setSuccess(false);
+        (!isset($json_data->username) ? $response->addMessage('Username not provided.') : false);
+        (!isset($json_data->password) ? $response->addMessage('Password not provided.') : false);
+        $response->send();
+        exit;   
+    }
+
+}
+else{
+    $response = new Response();
+    $response->setHttpStatusCode(404);
+    $response->setSuccess(false);
+    $response->addMessage('Endpoint not found.');
+    $response->send();
+    exit;    
+}
